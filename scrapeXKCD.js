5e6506f28cb821f9037e47b262d1dfd2c42ba232
Ivan.Reimand <145450323+IvanReimand@users.noreply.github.com>
2025-05-19 21:11:15 +0300

    Update scrapeXKCD.js

--- scrapeXKCD.js
+++ scrapeXKCD.js
@@ -1,35 +1,35 @@
 import axios from "axios";
 import * as cheerio from 'cheerio';
 import fs from 'fs';
-const sleep = function(ms){
-    return new Promise(resolve => setTimeout(resolve, ms));
+const sleep = function(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-fs.mkdirSync('cache');
+if(!fs.existsSync('cache')){
+    fs.mkdirSync('cache');
+}
 
 const cacheGet = (name) => {
     if(fs.existsSync('cache/' + name + '.html')){
         return fs.readFileSync('cache/' + name + '.html');
-    }
+    } 
     return false;
 }
 
-
-
-const cacheSet = (name) => {
-    fs.existsSync('cache/' + name + '.html')
+const cacheSet = (name, value) => {
+   fs.writeFileSync('cache/' + name + '.html', value);
 }
 
-for (let i=3088; i>3078; i--){
+for(let i = 3088; i>3078; i--){
     let data = cacheGet(i)
     if(!data){
         await sleep(1000);
-        console.log('!!! live data');
-        let res = await axios.get('https://xkcd.com/${i}/');
+        console.log('!!!!! LIVE DATA');
+        let res = await axios.get(`https://xkcd.com/${i}/`);
         data = res.data;
         cacheSet(i, data);
     }
-    const $ = cheerio.load(res.data);
+    const $ = cheerio.load(data);
     let src = $('#comic img').attr('src');
     let title = $('#comic img').attr('alt');
     let text = $('#comic img').attr('title');
